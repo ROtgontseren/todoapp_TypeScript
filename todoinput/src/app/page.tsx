67147624758146403e7a4ffdf6ea486658ae9941
Otgontseren Radnaabazar <LP230109@G301-CC0180.local>
2024-01-10 18:37:28 +0800

    todo app zaswar 1

--- todoinput/src/app/page.tsx
+++ todoinput/src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
   return (
     <main className="flex justify-center w-screen h-screen items-center bg-slate-400">
-      <div className="flex flex-col items-center gap-4 w-[600px] h-[150px] rounded-2xl bg-white">
+      <div className="flex flex-col items-center gap-4 w-[600px] h-[600px] rounded-2xl bg-white">
         <h1>todo application</h1>
         <p>Count :{count}</p>
         <div className="flex gap-4">
@@ -23,6 +23,24 @@ export default function Home() {
           <button className="bg-green-400 rounded-full w-12" onClick={sub}>
             +1
           </button>
+        </div>
+        <div className="flex gap-12">
+          <div className="w-[150px] h-[300px] bg-slate-200 rounded-3xl px-6 flex justify-between flex-row">
+            To Do
+            <button className="bg-green-400 w-6 h-6 rounded-full">+</button>
+            <hr className="" />
+          </div>
+          <div className="w-[150px] h-[300px] bg-slate-200 rounded-3xl px-6 flex items-center flex-col">
+            <h1>In Progress</h1>
+            <div className="flex flex-col rounded-3xl w-[120px] bg-white p-2">
+              <h1>static</h1>
+              <h1>artur</h1>
+              <h1>date added</h1>
+            </div>
+          </div>
+          <div className="w-[150px] h-[300px] bg-slate-200 rounded-3xl px-6">
+            Done
+          </div>
         </div>
       </div>
     </main>
